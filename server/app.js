d5ab10824108d77c40ddd58fb2cc4d128fb37d3b
Sampreeth Immidisetty <sampreeth.immidisetty@gmail.com>
2023-06-19 11:08:54 +0530

    Creating backend app

--- server/app.js
+++ server/app.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.listen(3001, () => {
+  console.log("Server Running");
+});
