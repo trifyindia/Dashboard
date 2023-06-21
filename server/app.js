41ad798503711ae9262128950603d2999058e8ac
Sampreeth Immidisetty <sampreeth.immidisetty@gmail.com>
2023-06-21 12:51:01 +0530

    Integrated battery post data

--- server/app.js
+++ server/app.js
@@ -2,9 +2,25 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
-app.listen(3001, () => {
+app.post("/postTemperature", async (req, res) => {
+  console.log("Temperature: " + req.body.temperature);
+});
+
+app.post("/postBatteryData", async (req, res) => {
+  console.log("Here1");
+  console.log(req.body);
+  res.send("Success");
+});
+
+app.get("/testGet", async (req, res) => {
+  console.log("Get request");
+  res.send("Hello World");
+});
+
+app.listen(8446, () => {
   console.log("Server Running");
 });
